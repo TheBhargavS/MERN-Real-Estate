36ebfb64e7029c7b588c107992dd01978cb9f6e1
Bhargav <shelby99murphy@gmail.com>
2024-07-09 17:39:45 +0530

    signIn route and custom error handler

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
 
 dotenv.config()
 const mongooseConnection = mongoose.connect(process.env.MONGO)
@@ -12,7 +16,23 @@ const mongooseConnection = mongoose.connect(process.env.MONGO)
 })
 
 const app = express()
+app.use(cors())
+
 
 app.listen(3000, ()=>{
   console.log(`Server is running on port: 3000`)
+})
+app.use(express.json())
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+
+    return res.status(statusCode).json({
+      success:false,
+      statusCode,
+      message
+    })
 })
