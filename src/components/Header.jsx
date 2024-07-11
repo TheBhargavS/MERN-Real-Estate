6aff93188939209965f74075bad38f4de84b5abb
Bhargav <shelby99murphy@gmail.com>
2024-07-11 16:02:53 +0530

    reduxtoolkit and redux-persist

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -28,7 +28,7 @@ const Header = ()=>{
           <Link to="/about">
           <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
           </Link>
-           <Link to="contact">
+           <Link to="sign-in">
            <li className=' text-slate-700 hover:underline'>Sign in</li>
            </Link>
            
