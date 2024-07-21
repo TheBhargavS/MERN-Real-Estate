fd1bab075bf7cab2441419da256a4fcec600c086
Bhargav <shelby99murphy@gmail.com>
2024-07-21 13:50:17 +0530

    controller for OAUTH

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const signup = async(req, res, next)=>{
   
 }
 
-export const signIn = async(req, res, next)=>{
+export const signin = async(req, res, next)=>{
     
     const {email, password} = req.body
      try{
@@ -30,14 +30,46 @@ export const signIn = async(req, res, next)=>{
             const token = JWT.sign({id: validUser._id}, process.env.JWT_SECRET )
              const {password:pass, ...rest} = validUser._doc 
 
-            res
-               .cookie("acess_token", token, {httpOnly:true})
+            res .cookie("acess_token", token, {httpOnly:true})
                .status(200)
                .json(rest)
      }catch(error){
         next(error) 
      }
     
+
+
+}
+
+export const google = async(req, res, err)=>{
+    
+      try{
+       const user  =   await User.findOne({email: req.body.email})
+          if(user){
+              const token = JWT.sign({id:user._id}, process.env.JWT_SECRET)
+              const {password:pass, ...rest } = user._doc
+              res
+                 .cookie("access_token",  token,  {httpOnly:true})
+                 .status(200)
+                 .json(rest)
+          }else{
+             const generatedPassword = Math.random().toString(36).slice(-8)  + Math.random().toString(36).slice(-8)
+             const hashedPassword    = bcrypt.hashSync(generatedPassword, 10)
+             const newUser  =   new User({username:req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email:req.body.email, password:hashedPassword, avatar:req.body.photo })
+                await newUser.save()
+
+               const token = JWT.sign({id:newUser._id}, process.env.JWT_SECRET) 
+               const {password:pass, ...rest}   = newUser._doc
+               res 
+                  .cookie("access_token", token, {httpOnly:true})
+                  .status(200)
+                  .json(rest)
+          } 
+      }catch(error){
+         next(error)
+      }
+
+
 
 
 }
--- models/user.model.js
+++ models/user.model.js
@@ -14,7 +14,11 @@ const userSchema = new mongoose.Schema({
      password:{
          type:String,
          required:true,
-     }
+     },
+     avatar:{
+        type:String,
+        default:"https://th.bing.com/th/id/OIP.H3Xxvz-5yotdS4Xr6SfnmwHaHa?w=202&h=202&c=7&r=0&o=5&dpr=1.7&pid=1.7"
+     },
 
 }, {timestamps:true} )
 
